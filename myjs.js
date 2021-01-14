160c6247f4fffac4b0abbb9ff1bb2e201a54f5e4
Denis Jojot <denis.jojot.ferreira@gmail.com>
2021-01-14 14:58:49 -0500

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -5,18 +5,25 @@ button.addEventListener("click", function () {
   x = document.getElementById("input").value;
   let splitStr = x.split(" ");
   // Set starting speed 100wpm = .6 seconds
-  let startSpeed = 200;
+  let startSpeed = 150;
   let i = 0;
 
-  let interval = setInterval(() => {
-    document.getElementById("output").innerHTML = splitStr[i];
-    i++;
-    if (i >= splitStr.length) {
-      i = 0;
-    }
-  }, startSpeed);
-
-  stopButton.addEventListener("click", () => {
-    clearInterval(interval);
-  });
+  fetch("https://bible-api.com/romans+2")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.text);
+      splitStr = data.text.replace(/[\W_]+/g, " ").split(" ");
+      let interval = setInterval(() => {
+        document.getElementById("output").innerHTML = splitStr[i];
+        i++;
+        if (i >= splitStr.length) {
+          clearInterval(interval);
+        }
+      }, startSpeed);
+      stopButton.addEventListener("click", () => {
+        clearInterval(interval);
+      });
+    });
 });
