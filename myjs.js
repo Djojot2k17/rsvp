bd9e99aad10453b202af7d28f243839dcb103af5
Denis Jojot <denis.jojot.ferreira@gmail.com>
2021-01-15 09:53:37 -0500

    Got a fetch method going

--- myjs.js
+++ myjs.js
@@ -1,29 +1,46 @@
 let x;
 let button = document.getElementById("convert");
+let john = document.getElementById("john");
 let stopButton = document.getElementById("stop");
+let startSpeed = 200;
 button.addEventListener("click", function () {
   x = document.getElementById("input").value;
   let splitStr = x.split(" ");
-  // Set starting speed 100wpm = .6 seconds
-  let startSpeed = 150;
   let i = 0;
 
-  fetch("https://bible-api.com/romans+2")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.text);
-      splitStr = data.text.replace(/[\W_]+/g, " ").split(" ");
+  let interval = setInterval(() => {
+    document.getElementById("output").innerHTML = splitStr[i];
+    i++;
+    if (i >= splitStr.length) {
+      clearInterval(interval);
+    }
+  }, startSpeed);
+
+  stopButton.addEventListener("click", () => {
+    clearInterval(interval);
+  });
+});
+
+let fetchedVar;
+
+fetch(`https://bible-api.com/genesis+1`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    fetchedVar = data.text.replace(/[\W_]+/g, " ").split(" ");
+    john.addEventListener("click", () => {
+      let i = 0;
       let interval = setInterval(() => {
-        document.getElementById("output").innerHTML = splitStr[i];
+        document.getElementById("output").innerHTML = fetchedVar[i];
         i++;
-        if (i >= splitStr.length) {
+        if (i >= fetchedVar.length) {
           clearInterval(interval);
         }
       }, startSpeed);
+
       stopButton.addEventListener("click", () => {
         clearInterval(interval);
       });
     });
-});
+  });
